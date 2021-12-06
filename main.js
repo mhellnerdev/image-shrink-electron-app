424b8fd7c6f6d18f17db8b228d4e40fc5d94e19a
Mark Hellner <mhellnerdev@gmail.com>
2021-12-05 21:10:38 -0500

    adding global shortcuts for dev tools and reload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
 
 // Set env
 // =========================================
@@ -30,6 +30,11 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
 
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+    mainWindow.toggleDevTools()
+  )
+
   mainWindow.on('closed', () => (mainWindow = null))
 })
 
@@ -44,6 +49,9 @@ const menu = [
     submenu: [
       {
         label: 'Quit',
+        // accelerator: isMac ? 'Command + W' : 'Ctrl+W',
+        accelerator: 'CmdOrCtrl+W',
+
         click: () => app.quit(),
       },
     ],
