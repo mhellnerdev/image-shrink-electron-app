d9fbe5691451c03a96fa8645c29602b54e8b7586
Mark Hellner <mhellnerdev@gmail.com>
2021-12-05 23:31:44 -0500

    file size reduction working

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+const path = require('path')
+const os = require('os')
 const {
   app,
   BrowserWindow,
   Menu,
   globalShortcut,
   ipcMain,
+  shell,
 } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
 
 // Set env
 // =========================================
@@ -118,8 +125,30 @@ const menu = [
 ]
 
 ipcMain.on('image:minimize', (e, options) => {
-  console.log(options)
+  options.dest = path.join(os.homedir(), 'imageshrink')
+  shrinkImage(options)
 })
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    })
+
+    console.log(files)
+
+    shell.openPath(dest)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 // on mac osx keeps closed application from quitting which is typical of what you expect on OSX
 // =========================================
