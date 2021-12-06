6fc87b0b1993f88fc5c0e12a19678650eaab042e
Mark Hellner <mhellnerdev@gmail.com>
2021-12-05 20:00:41 -0500

    loadFile use

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function createMainWindow() {
     height: 600,
   })
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`) // Commented out for use of .loadFile function below
+  mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
